720c107294b7a72380d156cec65b3d89ebb7973e
Noah Lutz <noah.l98@gmail.com>
2014-03-04 13:39:24 -0500

    Updated

--- build/source/r/debug/com/noahl98/jotmobile/R.java
+++ build/source/r/debug/com/noahl98/jotmobile/R.java
@@ -40,11 +40,11 @@ public final class R {
         public static final int separator=0x7f02000f;
     }
     public static final class id {
-        public static final int action_settings=0x7f090014;
+        public static final int action_settings=0x7f090013;
         public static final int alignment=0x7f09000d;
         public static final int bold=0x7f090008;
         public static final int container=0x7f09000e;
-        public static final int docTitle=0x7f090012;
+        public static final int docTitle=0x7f090011;
         public static final int drawer_layout=0x7f090001;
         public static final int edit_text=0x7f090004;
         public static final int formatBar=0x7f090007;
@@ -53,13 +53,12 @@ public final class R {
         public static final int ital=0x7f090009;
         public static final int left_drawer=0x7f090005;
         public static final int right_drawer=0x7f090006;
-        public static final int save=0x7f090013;
+        public static final int save=0x7f090012;
         public static final int scrollView=0x7f090003;
         public static final int separator=0x7f09000c;
         public static final int strike=0x7f09000b;
         public static final int text1=0x7f090010;
         public static final int title_box=0x7f090002;
-        public static final int title_title=0x7f090011;
         public static final int underline=0x7f09000a;
     }
     public static final class layout {
