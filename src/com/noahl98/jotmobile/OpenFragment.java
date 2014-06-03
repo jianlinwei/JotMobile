b4127e4954db12736997841e00b9c6aad68c8edd
Noah Lutz <noah.l98@gmail.com>
2014-06-03 13:02:52 -0400

    Added OpenFragment functionality

---
+++ src/com/noahl98/jotmobile/OpenFragment.java
@@ -0,0 +1,17 @@
+package com.noahl98.jotmobile;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by noahl98 on 3/31/14.
+ */
+public class OpenFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+    }
+}
